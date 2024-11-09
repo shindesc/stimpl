a3fa7990ff08123046e97e57f66028d214841b01
shindesc <sarashinde2061@gmail.com>
2024-11-08 19:12:57 -0500

    Update runtime.py

--- stimpl/runtime.py
+++ stimpl/runtime.py
@@ -173,27 +173,28 @@ def evaluate(expression: Expr, state: State) -> Tuple[Optional[Any], Type, State
             pass
 
         case Divide(left=left, right=right):
-            """ TODO: Implement. """
-
+        """ TODO: Implement. """
+    
             left_result, left_type, new_state = evaluate(left, state)
             right_result, right_type, new_state = evaluate(right, new_state)
-
-        if left_type != right_type:
-            raise InterpTypeError(f"Mismatched types for Divide: Cannot divide {left_type} by {right_type}")
-
-        match left_type:
-            case Integer():
-                if right_result == 0:
-                    raise InterpMathError("Division by zero")
-                result = left_result / right_result
-            case FloatingPoint():
-                if right_result == 0.0:
-                    raise InterpMathError("Division by zero")
-                result = left_result / right_result
-            case _:
-                raise InterpTypeError(f"Cannot divide {left_type}s")
-
+        
+            if left_type != right_type:
+                raise InterpTypeError(f"Mismatched types for Divide: Cannot divide {left_type} by {right_type}")
+        
+            match left_type:
+                case Integer():
+                    if right_result == 0:
+                        raise InterpMathError("Division by zero")
+                    result = left_result / right_result
+                case FloatingPoint():
+                    if right_result == 0.0:
+                        raise InterpMathError("Division by zero")
+                    result = left_result / right_result
+                case _:
+                    raise InterpTypeError(f"Cannot divide {left_type}s")
+        
             return (result, left_type, new_state)
+
             
             pass
 
